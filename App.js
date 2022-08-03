d150fcb8f9dacf159acaf2ee76d9cdf2c9e922d3
Fatih Eren Özçelik <eren.ozcelik@std.antalya.edu.tr>
2022-08-03 11:33:29 +0300

    Eren first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Beginning of the project</Text>
+    </View>
+  );
+};
+
+export default App;
